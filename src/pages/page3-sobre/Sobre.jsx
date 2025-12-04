3028259225346b0fb6a6802a9e05094dfb279b68
Kleber Lucas.G.S. <lenovomicrosof1@gmail.com>
2025-12-04 13:43:23 -0300

    termino feat seção abordagem

--- src/pages/page3-sobre/Sobre.jsx
+++ src/pages/page3-sobre/Sobre.jsx
@@ -1,11 +1,13 @@
 import Nav from "../../components/Nav";
 import Intro from "../page3-sobre/layout/Intro";
+import Abordagem from "./layout/Abordagem";
 
 function Sobre() {
     return (
         <>
             <Nav />
-            <Intro/>
+            <Intro />
+            <Abordagem/>
         </>
     )
 }
