756d90d9473c78f7c211fe3d0712f9571724e917
Kleber Lucas.G.S. <lenovomicrosof1@gmail.com>
2025-12-16 14:49:46 -0300

    refactor: removei o estado desnecessário e componente de barra de rolagem

--- src/pages/page1/autono.jsx
+++ src/pages/page1/autono.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import Lenis from "lenis"; // Importe do Lenis
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
@@ -13,14 +13,12 @@ import Parceiros from "../page1/layout/Parceiros";
 import Carreira from "../page1/layout/Carreira";
 import Rodape from "/src/components/Rodape";
 
-import ScrollProgressBar from "/src/components/Scroll-bar";
 
 gsap.registerPlugin(ScrollTrigger);
 
 function Autono() {
     // ✅ ESSAS LINHAS ESTAVAM FALTANDO
     const lenisRef = useRef(null);
-    const [isReady, setIsReady] = useState(false);
 
     useEffect(() => {
         const start = () => {
@@ -38,7 +36,6 @@ function Autono() {
             const raf = (time) => lenis.raf(time * 1000);
             gsap.ticker.add(raf);
 
-            setIsReady(true);
         };
 
         if (document.readyState === "complete") {
@@ -53,7 +50,6 @@ function Autono() {
     return (
         <>
             <Nav />
-            {isReady && <ScrollProgressBar lenisRef={lenisRef} />}
             <Hero />
             <Sobre />
             <Servicos />
